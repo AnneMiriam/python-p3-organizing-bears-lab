1732361e0810ce4019bd87dca069f4ad155aed2e
Anne Hastings <anne.miriam@gmail.com>
2023-12-19 17:32:50 -0700

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,47 @@
+INSERT INTO bears (
+name, age, sex, color, temperament, alive
+) VALUES (
+'Mr. Chocolate', 15, 'M', 'brown', 'easy going', 1
+);
+
+INSERT INTO bears (
+name, age, sex, color, temperament, alive
+) VALUES (
+'Rowdy', 1, 'M', 'black and tan', 'playful', 1
+);
+
+INSERT INTO bears (
+name, age, sex, color, temperament, alive
+) VALUES (
+'Tabitha', 6, 'F', 'black', 'sneaky', 1
+);
+
+INSERT INTO bears (
+name, age, sex, color, temperament, alive
+) VALUES (
+'Sergeant Brown', 9, 'F', 'brown', 'kind of a B', 1
+);
+
+INSERT INTO bears (
+name, age, sex, color, temperament, alive
+) VALUES (
+'Melissa', 3, 'F', 'cinnamon', 'sweet', 1
+);
+
+INSERT INTO bears (
+name, age, sex, color, temperament, alive
+) VALUES (
+'Grinch', 19, 'M', 'green', 'salty', 1
+);
+
+INSERT INTO bears (
+name, age, sex, color, temperament, alive
+) VALUES (
+'Wendy', 12, 'F', 'black and white', 'lazy', 1
+);
+
+INSERT INTO bears (
+name, age, sex, color, temperament, alive
+) VALUES (
+NULL, NULL, 'F', 'chestnut', 'fearful', 1
+);
